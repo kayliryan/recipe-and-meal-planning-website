f9ac68c1e49ff5b60353c8d662bb9e9a0554dfbd
Kayli <kayliryan@gmail.com>
2022-06-07 18:52:43 -0700

    Added authors to recipes on create and edit

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,17 +1,5 @@
 from django import forms
-from recipes.models import Recipe
 from recipes.models import Rating
-
-
-class RecipeForm(forms.ModelForm):
-    class Meta:
-        model = Recipe
-        fields = [
-            "name",
-            "author",
-            "description",
-            "image",
-        ]
 
 
 class RatingForm(forms.ModelForm):
