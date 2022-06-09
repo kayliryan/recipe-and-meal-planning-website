57940db4a8149b733e1dbd09d567b97e9d995cd5
Kayli <kayliryan@gmail.com>
2022-06-08 21:17:13 -0700

    detail page finally working and displaying everything. only edit link not linking properly

--- meal_plans/urls.py
+++ meal_plans/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
 
-from meal_plans.views import MealPlanListView
+from meal_plans.views import (
+    MealPlanListView,
+    MealPlanCreateView,
+    MealPlanDetailView,
+    MealPlanUpdateView,
+    MealPlanDeleteView,
+)
 
-# MealPlanDetailView,
-# MealPlanCreateView,
-# MealPlanUpdateView,
-# MealPlanDeleteView,
 
 urlpatterns = [
     path("", MealPlanListView.as_view(), name="meal_plans_list"),
+    path("create/", MealPlanCreateView.as_view(), name="meal_plan_new"),
+    path("<int:pk>/", MealPlanDetailView.as_view(), name="meal_plan_detail"),
+    path("<int:pk>/edit/", MealPlanUpdateView.as_view(), name="meal_plan_edit"),
+    path("<int:pk>/delete/", MealPlanDeleteView.as_view(), name="meal_plan_delete"),
 ]
 
-# path("create/", MealPlanCreateView.as_view(), name="meal_plan_new"),
-# path("<int:pk>/", MealPlanDetailView.as_view(), name="meal_plan_detail"),
-# path("<int:pk>/edit/", MealPlanUpdateView.as_view(), name="meal_plan_edit"),
-# path("<int:pk>/delete/", MealPlanDeleteView.as_view(),
-# name="meal_plan_delete"),
--- meal_plans/views.py
+++ meal_plans/views.py
@@ -1,10 +1,14 @@
 # from django.shortcuts import render
+from django.shortcuts import redirect
 from meal_plans.models import MealPlan
+from django.urls import reverse_lazy
 
 from django.views.generic.list import ListView
 
-# from django.views.generic.detail import DetailView
-# from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+# , DeleteView, UpdateView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -12,12 +16,49 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class MealPlanListView(LoginRequiredMixin, ListView):
     model = MealPlan
     template_name = "meal_plans/list.html"
-    paginate_by = 7
-
-    # def get(self, request):
-    #     return self.render_to_response({})
+    context_object_name = "meal_plans_list"
+    paginate_by = 2
 
 
-# class MealPlanDetailView(DetailView):
-#     model = MealPlan
-#     template_name = "meal_plans/detail.html"
+class MealPlanCreateView(LoginRequiredMixin, CreateView):
+    model = MealPlan
+    template_name = "meal_plans/new.html"
+    fields = ["name", "date", "recipes"]
+    success_url = reverse_lazy("meal_plan_detail")
+    # don't know if above is right. may need to redirect
+
+    def form_valid(self, form):
+        # Save the meal plan, but don't put it in the database
+        plan = form.save(commit=False)
+        # Assign the owner to the meal plan
+        plan.owner = self.request.user
+        # Now, save it to the database
+        plan.save()
+        # Save all of the many-to-many relationships
+        form.save_m2m()
+        # Redirect to the detail page for the meal plan
+        return redirect("meal_plan_detail", pk=plan.id)
+
+    def get_queryset(self):
+        return MealPlan.objects.filter(owner=self.request.user)
+
+
+class MealPlanDetailView(LoginRequiredMixin, DetailView):
+    model = MealPlan
+    template_name = "meal_plans/detail.html"
+    fields = ["name", "date", "recipes"]
+
+    def get_queryset(self):
+        return MealPlan.objects.filter(owner=self.request.user)
+
+
+class MealPlanUpdateView(LoginRequiredMixin, UpdateView):
+    model = MealPlan
+    template_name = "meal_plans/edit.html"
+    success_url = reverse_lazy("meal_plans_list")
+
+
+class MealPlanDeleteView(LoginRequiredMixin, DeleteView):
+    model = MealPlan
+    template_name = "meal_plans/delete.html"
+    success_url = reverse_lazy("meal_plans_list")
